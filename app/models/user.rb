dc11534d53751cdbace7b1b07248911e85f92b6a
JavierIbarra <jaibarra1@miuandes.cl>
2019-05-16 20:59:10 -0400

    OmniauthCallbacksController

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, 
          :omniauthable, omniauth_providers: %i[facebook]
-         
+
+    def self.from_omniauth(auth)
+	  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+	    user.email = auth.info.email
+	    user.password = Devise.friendly_token[0, 20]
+	    user.name = auth.info.name   # assuming the user model has a name
+	  end
+	end
+
   	has_many :user_tickets	
 	has_many :tickets, :through => :user_tickets
 
