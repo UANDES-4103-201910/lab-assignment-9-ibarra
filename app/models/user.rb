41fe7d2f62a63dab30c0ce02b68c2e3259efbc8f
JavierIbarra <jaibarra1@miuandes.cl>
2019-05-16 20:48:45 -0400

    gem devise and omniauth-facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, 
+         :omniauthable, omniauth_providers: %i[facebook]
+         
   	has_many :user_tickets	
 	has_many :tickets, :through => :user_tickets
 
